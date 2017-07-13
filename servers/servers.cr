80c29ed08a9d575723fd46009d9ac1244a5916a3
Luke Sweeney <luke@thesweeneys.org>
2017-07-13 18:49:51 -0500

    It worked

--- servers/servers.cr
+++ servers/servers.cr
@@ -10,11 +10,14 @@ else
   args.shift
 end
 
+servers = {} of String => Tuple(String, String)
+
 storage = File.expand_path("~/.servers.yml")
+
 unless File.empty?(storage)
-  servers = YAML.parse(File.read(storage)).as_h
-else
-  servers = {} of String => String
+  YAML.parse(File.read(storage)).each do |name, address|
+    servers[name.to_s] = {address[0].to_s, address[1].to_s}
+  end
 end
 
 case command
@@ -26,7 +29,7 @@ when "add"
   print "Server IP: "
   ip = gets
 
-  servers[name.to_s] = "#{username.to_s}@#{ip.to_s}"
+  servers[name.to_s] = {username.to_s, ip.to_s}
 
   # Store details
   File.open(storage, "w") do |f|
@@ -36,17 +39,32 @@ when "list"
   server_table = TerminalTable.new
   server_table.headings = ["Name", "Address"]
   servers.each do |name, ip|
-    server_table << [name.to_s, ip.to_s]
+    server_table << [name.to_s, ip[0].to_s + "@" + ip[1].to_s]
   end
   puts server_table.render
 when "help"
   puts <<-help
-    servers
-    servers list   :   List all stored servers
-    servers add    :   Store a new server
+    <-- servers -->
+
+    Commands:
+    servers list          :   List all stored servers
+    servers add           :   Store a new server
+    server connect [name] :   Connect to a server
+
     ---
+
     TIP: run
       servers list | grep [name]
     to search for a specific server
+
+    <---->
   help
+when "connect"
+  server_name = args[0] unless args.empty?
+  if !server_name
+    print "Please enter a server name: "
+    server_name = gets.to_s
+  end
+  server = servers[server_name]
+  system("ssh #{server[0]}@#{server[1]}")
 end
